b26d29e452d9077bff9e807e2bf9e9d16a74fcba
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2017-01-26 20:38:13 +0000

    change rootURL and set locationtype to hash for GHP

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/lilypond-wui/';
+    ENV.rootURL = '/lilypond/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
